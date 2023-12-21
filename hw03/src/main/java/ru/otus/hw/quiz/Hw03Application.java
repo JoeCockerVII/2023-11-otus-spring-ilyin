0f7a6ff86118b0dc6d0ec8e28e05d822110ff4fa
Ilya Ilyin <ilinimm@ya.ru>
2023-12-22 02:15:39 +0300

    hw03: update description, add app template

---
+++ hw03/src/main/java/ru/otus/hw/quiz/Hw03Application.java
@@ -0,0 +1,13 @@
+package ru.otus.hw.quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw03Application.class, args);
+    }
+
+}
---
+++ hw03/src/test/java/ru/otus/hw/quiz/Hw03ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.hw.quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
