84e8b02badab63a4c1c5ebe707e91d457d561bab
Ilya Ilyin <ilinimm@ya.ru>
2023-12-27 23:38:28 +0300

    hw03: code review fixes

--- hw03/src/main/java/ru/otus/hw/quiz/service/impl/LocalizedMessagesServiceImpl.java
+++ hw03/src/main/java/ru/otus/hw/quiz/service/impl/LocalizedMessagesServiceImpl.java
@@ -1,21 +1,24 @@
 package ru.otus.hw.quiz.service.impl;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.quiz.config.LocaleConfig;
 import ru.otus.hw.quiz.service.LocalizedMessagesService;
 
-@RequiredArgsConstructor
 @Service
 public class LocalizedMessagesServiceImpl implements LocalizedMessagesService {
 
     private final LocaleConfig localeConfig;
 
+    private final ReloadableResourceBundleMessageSource messageSource;
+
+    public LocalizedMessagesServiceImpl(LocaleConfig localeConfig) {
+        this.localeConfig = localeConfig;
+        this.messageSource = new ReloadableResourceBundleMessageSource();
+    }
+
     @Override
-    public String getMessage(String code, Object[] args) {
-        ReloadableResourceBundleMessageSource messageSource
-                = new ReloadableResourceBundleMessageSource();
+    public String getMessage(String code, Object... args) {
         messageSource.setBasename(localeConfig.getBaseName());
         messageSource.setDefaultEncoding(localeConfig.getEncoding());
         return messageSource.getMessage(code, args, localeConfig.getLocale());
